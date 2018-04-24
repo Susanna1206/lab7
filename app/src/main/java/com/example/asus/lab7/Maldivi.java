63c325e7473db84899d7bc3dde3c82b642c05c94
Susanna1206 <tanisheva.s.1.2.16@gmail.com>
2018-04-24 19:05:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/asus/lab7/Maldivi.java
@@ -0,0 +1,11 @@
+package com.example.asus.lab7;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Maldivi extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_canari);
+    }
+}
